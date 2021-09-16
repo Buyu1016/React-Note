e2fc135087ab5e934fac534e22d1d6b3403726cb
CodeGorgeous <2460481461@qq.com>
2021-09-16 22:56:43 +0800

    提交

--- React核心/reactdemo/src/MyClassComponent.jsx
+++ React核心/reactdemo/src/MyClassComponent.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 // 必须继承React.Component
 export default class MyClassComponent extends React.Component {
-    
+    // 可以默认不写的
+    // constructor(props) {
+    //     super(props);
+    // }
     // 必要返回一个React对象
     render() {
-        return <h1>Hello React Class Component</h1>
+        return this.props.ui ? this.props.ui : <h1>Hello React Class Component, {this.props.number}</h1>
     }
 }
--- React核心/reactdemo/src/MyFunComponent.jsx
+++ React核心/reactdemo/src/MyFunComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MyClassComponent from './MyClassComponent'
+
 // 首字母必须大写, 否则React会认为这是个普通的React对象
-export function MyFunComponents(){
-    return <h1>Hello React Function Component, <MyClassComponent/></h1>
+export function MyFunComponents(props){
+    return <h1>Hello React Function Component, {props.number}</h1>
 }
