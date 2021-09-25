dbc4e851a809bb6bc5864e1310ba9aeb527204fc
CodeGorgeous <2460481461@qq.com>
2021-09-25 21:57:45 +0800

    提交

--- React核心/reactdemo/src/App.jsx
+++ React核心/reactdemo/src/App.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
-import Carousel from './components/Carousel/index'
+
+function A(props, ref) {
+    return <h1 ref={ref}>毛毛</h1>
+}
+
+const NewA = React.forwardRef(A)
+
+class B extends React.Component {
+    render() {
+        return (
+            <div ref={this.props.BRef}>
+                帅帅
+            </div>
+        )
+    }
+}
+
 export default class App extends Component {
     constructor(props) {
         super(props)
         this.state = {}
+        this.ARef = React.createRef()
+        this.BRef = React.createRef()
     }
+    componentDidMount() {
+        console.log(this.ARef, this.BRef)
+    }
+    
     render() {
         return (
             <div>
-                <Carousel data={[
-                    "http://qiniu.codegorgeous.top/top.png",
-                    "https://img2.baidu.com/it/u=1109821744,1980699124&fm=26&fmt=auto",
-                    "http://qiniu.codegorgeous.top/top.png"
-                ]}/>
+                Hello
+                <NewA ref={this.ARef} />
+                <B BRef={this.BRef}/>
             </div>
         )
     }
